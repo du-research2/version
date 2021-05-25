d27ddeec0871d4b5e5ebd4eed72d26ff1360bb82
du-research2 <you@example.com>
2021-05-25 17:53:40 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/du-research2
+
+go 1.16
---
+++ version.go
@@ -0,0 +1,11 @@
+package version
+
+import "fmt"
+
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+    // Return a greeting that embeds the name in a message.
+    message := fmt.Sprintf("Demo for Tzahi, %v. Welcome Back Again!", name)
+    return message
+}
+
