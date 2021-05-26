7ae60b770b60a949bc9cc0e8161173c7642e19c0
du-research2 <you@example.com>
2021-05-26 07:23:27 +0000

    change text

--- version.go
+++ version.go
@@ -5,7 +5,7 @@ import "fmt"
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
     // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf("Demo for Tzahi zz, %v. Welcome Back Again!", name)
+    message := fmt.Sprintf("Demo for Tzahi zzz, %v. Welcome Back Again!", name)
     return message
 }
 
