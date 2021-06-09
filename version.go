166be66029f9731cbf6c76ae90fda6c81b74ef50
du-research2 <84804542+du-research2@users.noreply.github.com>
2021-06-09 15:17:02 +0300

    Update version.go

--- version.go
+++ version.go
@@ -1,11 +1,15 @@
 package version
 
-import "fmt"
+import (
+    "fmt"
+    "github.com/teris-io/shortid"
 
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
     // Return a greeting that embeds the name in a message.
     message := fmt.Sprintf("Demo for Tzahi zzz, %v. Welcome Back Again!", name)
+    fmt.Printf(shortid.Generate())
     return message
 }
+
 
